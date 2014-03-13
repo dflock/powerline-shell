296b634837ade95f34a928287fb4095351103684
Stephen Miller <qwindelzorf@gmail.com>
2014-03-12 22:48:16 -0700

    fixes for python 3, and pyflakes

--- themes/default.py
+++ themes/default.py
@@ -11,23 +11,23 @@ class DefaultColor:
     HOSTNAME_BG = 238
 
     HOME_SPECIAL_DISPLAY = True
-    HOME_BG = 31  # blueish
-    HOME_FG = 15  # white
-    PATH_BG = 237  # dark grey
-    PATH_FG = 250  # light grey
-    CWD_FG = 254  # nearly-white grey
+    HOME_BG = 31            # blueish
+    HOME_FG = 15            # white
+    PATH_BG = 237           # dark grey
+    PATH_FG = 250           # light grey
+    CWD_FG = 254            # nearly-white grey
     SEPARATOR_FG = 244
 
     READONLY_BG = 124
     READONLY_FG = 254
 
-    SSH_BG = 166 # medium orange
+    SSH_BG = 166            # medium orange
     SSH_FG = 254
 
-    REPO_CLEAN_BG = 148  # a light green color
-    REPO_CLEAN_FG = 0  # black
-    REPO_DIRTY_BG = 161  # pink/red
-    REPO_DIRTY_FG = 15  # white
+    REPO_CLEAN_BG = 148     # a light green color
+    REPO_CLEAN_FG = 22      # dark green
+    REPO_DIRTY_BG = 161     # pink/red
+    REPO_DIRTY_FG = 15      # white
 
     JOBS_FG = 39
     JOBS_BG = 238
@@ -38,9 +38,9 @@ class DefaultColor:
     CMD_FAILED_FG = 15
 
     SVN_CHANGES_BG = 148
-    SVN_CHANGES_FG = 22  # dark green
+    SVN_CHANGES_FG = 22     # dark green
 
-    VIRTUAL_ENV_BG = 35  # a mid-tone green
+    VIRTUAL_ENV_BG = 35     # a mid-tone green
     VIRTUAL_ENV_FG = 00
 
 class Color(DefaultColor):
