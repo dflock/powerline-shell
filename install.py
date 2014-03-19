dc53cbc8faa4c267b60f712da12ab50903ad15f1
Stephen Miller <qwindelzorf@gmail.com>
2014-03-19 11:09:08 -0700

    Fixed bug causing default colors to be inserted into the output twice

--- install.py
+++ install.py
@@ -27,7 +27,8 @@ def load_source(srcfile):
 if __name__ == "__main__":
     source = load_source(TEMPLATE_FILE)
     source += load_source(os.path.join(THEMES_DIR, 'default.py'))
-    source += load_source(os.path.join(THEMES_DIR, config.THEME + '.py'))
+    if config.THEME != "default":
+        source += load_source(os.path.join(THEMES_DIR, config.THEME + '.py'))
     for segment in config.SEGMENTS:
         source += load_source(os.path.join(SEGMENTS_DIR, segment + '.py'))
     source += '''
