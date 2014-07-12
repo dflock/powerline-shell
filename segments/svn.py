8900ce786ae875f73ae54358bb7cf6481148f568
Stephen Miller <qwindelzorf@gmail.com>
2014-07-12 08:55:47 -0700

    Updating SVN segment to use proper colorization and add some useful info

--- segments/svn.py
+++ segments/svn.py
@@ -7,13 +7,34 @@ def add_svn_segment():
     if len(is_svn_output) != 0:
         return
 
+    p0 = subprocess.Popen(['svn', 'info'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    info_raw = p0.communicate()[0].strip()
+    info = {}
+    for pair in info_raw.split("\n"):
+        vals = pair.split(":")
+        k = vals[0]
+        v = vals[1]
+        info[k] = v.strip()
+    rev = ""
+    if info["Revision"]:
+        rev = info["Revision"]
+
     #"svn status | grep -c "^[ACDIMRX\\!\\~]"
     p1 = subprocess.Popen(['svn', 'status'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     p2 = subprocess.Popen(['grep', '-c', '^[ACDIMR\\!\\~]'], stdin=p1.stdout, stdout=subprocess.PIPE)
     output = p2.communicate()[0].strip()
+    changes = ""
     if len(output) > 0 and int(output) > 0:
-        changes = output.strip()
-        powerline.append(' %s ' % changes, Color.SVN_CHANGES_FG, Color.SVN_CHANGES_BG)
+        changes = u" \u00B1 %s" % output.strip()
+
+    # Color indicates pending commits
+    bg = Color.REPO_CLEAN_BG
+    fg = Color.REPO_CLEAN_FG
+    if changes:
+        bg = Color.REPO_DIRTY_BG
+        fg = Color.REPO_DIRTY_FG
+
+    powerline.append(u' %s %s%s ' % (powerline.branch, rev, changes), fg, bg)
 
 try:
     add_svn_segment()
